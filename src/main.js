aa787b51e08088e4af9b354f38bf9143ab4a7900
Lee <lee.budaden@gmail.com>
2026-06-12 09:51:29 +0800

    removed user view

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import Admin from './Admin.vue'
-import User from './User.vue'
 import TreeNode from './components/TreeNodeComponent.vue'
 
-// Admin always in dev, User in production
-const component = import.meta.env.PROD ? User : Admin;
-
-const app = createApp(component)
+const app = createApp(Admin)
 app.component('tree-node', TreeNode)
 app.mount('#app')
